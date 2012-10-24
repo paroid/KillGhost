6714b12ca055f6ddf7444d326e5dfd8851c65e61
paroid <chenke625@gmail.com>
2012-10-25 00:22:59 +0800

    uptodate

--- server.php
+++ server.php
@@ -512,6 +512,6 @@ $Server->bind('open', 'wsOnOpen');
 $Server->bind('close', 'wsOnClose');
 // for other computers to connect, you will probably need to change this to your LAN IP or external IP,
 // alternatively use: gethostbyaddr(gethostbyname($_SERVER['SERVER_NAME']))
-$Server->wsStartServer('58.154.201.146', 9300);
+$Server->wsStartServer('127.0.0.1', 9300);
 
 ?>
